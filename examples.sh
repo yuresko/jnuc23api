789fc26a546eb50d58dcc49f3cd2317ca9925931
yuresko <jyuresko@gmail.com>
2023-09-16 18:15:46 -0400

    Create examples.sh

---
+++ examples.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#GET /computers
+/usr/bin/curl -sku "username:password" https://instance.jamfcloud.com/JSSResource/computers
+
+#GET /mobiledevices
+/usr/bin/curl -sku "username:password" https://instance.jamfcloud.com/JSSResource/mobiledevices -X GET
+
+#### TASK ####
+# Try more GET API actions on other endpoints. See what information is 
+# returned and if it looks familiar to the GUI.
+
+
+
+#GET /mobiledevices/id/___
+/usr/bin/curl -sku "username:password" https://instance.jamfcloud.com/JSSResource/mobiledevices/id/1
+
+#### TASK ####
+# Try additional endpoints with alternate IDs.
+# Update the name of a policy via the GUI. Verify the name took place via API.
+
+
+
+#GET - Retrieve Serial Number of /mobiledevices/id/___
+/usr/bin/curl -sku "username:password" https://instance.jamfcloud.com/JSSResource/mobiledevices/id/1 | xmllint --xpath '/mobile_device/general/serial_number/text()' - 2>/dev/null
+
+#### TASK ####
+# Retrieve only the name of the policy you updated in the previous task.
+
+
+
+#PUT - Update Asset Tag for /mobiledevices/id/___
+/usr/bin/curl -sku "username:password" https://instance.jamfcloud.com/JSSResource/mobiledevices/id/1 -H "Content-type: application/xml" -X PUT -d "<mobile_device><general><asset_tag>ASSETTAG</asset_tag></general></mobile_device>"
+
+#### TASK ####
+# Assign an asset tag to a computer of your choice via API.
+# Verify the asset tag was changed appropriately via the API.
+# Change the name of the policy in the previous task via API.
+# Retrieve only the name of the policy you updated in the previous task.
